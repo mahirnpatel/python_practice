4f6e1c98c62f4d0a9c08a3d3ce1c8bff99ed8d89
mahir <mahirpatel3107@gmail.com>
2022-05-19 00:03:07 +0530

    Addition of two number

---
+++ addTwoNum.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number ==> "))
+num2 = int(input("Enter second number ==> "))
+
+result = num1 + num2
+# print("The addition of num1 and num2 ==> ",result)
+print("The addition of", num1 , "And" , num2 , "==>" , result)
