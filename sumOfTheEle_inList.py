94d1f689fc8b9bf811b32fa68f61b924fc125f88
mahir <mahirpatel3107@gmail.com>
2022-05-20 16:02:58 +0530

    Sum of the elements of the list

---
+++ sumOfTheEle_inList.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter number : "))
+num2 = int(input("Enter number : "))
+num3 = int(input("Enter number : "))
+num4 = int(input("Enter number : "))
+
+numList=[num1 , num2 , num3 , num4]
+
+print("The sum of the element in the List ==> ",sum(numList))
