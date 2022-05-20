014996d31a89199c70a5319c5fbc662ea8a8e327
mahir <mahirpatel3107@gmail.com>
2022-05-20 23:31:36 +0530

    My first Python code

---
+++ print_hello.py
@@ -0,0 +1,4 @@
+print('''Twinkle , twinkle , little , star,
+tHow i wonder what you are! aove the world so high,
+Like a diamond in the sky Twinkle , twinkle , little ,star,How i wonder what
+ you are  ''')
