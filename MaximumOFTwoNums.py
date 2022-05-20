802139bc387bb5438dde65ff684a7fac3923a89b
mahir <mahirpatel3107@gmail.com>
2022-05-20 16:07:08 +0530

    find max from thetwo number

---
+++ MaximumOFTwoNums.py
@@ -0,0 +1,11 @@
+# Input: a = 2, b = 4
+# Output: 4
+
+num1 = int(input("Enter number : ")) #Take number from user
+num2 = int(input("Enter number : ")) #Take number from user
+
+if num1 > num2 :
+    print(num1 , "is greater")
+
+else:
+    print(num2 , "is greater")
