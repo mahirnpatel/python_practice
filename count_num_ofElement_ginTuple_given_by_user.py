ab796339259491a5c97300b2c46f2f5815d9f6fa
mahir <mahirpatel3107@gmail.com>
2022-05-20 16:03:37 +0530

    count number of element in the tuple given by user

---
+++ count_num_ofElement_ginTuple_given_by_user.py
@@ -0,0 +1,5 @@
+numTuple=(1,0,2,0,5,0,6,87,8,15,9,0)
+
+user_numForCount = int(input("Enter the number to get count for that number : "))
+
+print("The frequency of " , user_numForCount , " ==> " , numTuple.count(user_numForCount))
