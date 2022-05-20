e6dc41c9b15e8cb006503a415f288b829b3ca4ad
mahir <mahirpatel3107@gmail.com>
2022-05-20 23:32:03 +0530

    Sum and average of the element of the list

---
+++ sum_and_average_inList.py
@@ -0,0 +1,12 @@
+# Input: [4, 5, 1, 2, 9, 7, 10, 8]
+# Output:
+# sum =  46
+# average =  5.75
+numList = [4, 5, 1, 2, 9, 7, 10, 8]
+
+sumOfElem = sum(numList)
+print("Sum of all elements ==> ",sumOfElem)
+
+
+avg = sumOfElem / len(numList)
+print("The average ==> ",avg)
