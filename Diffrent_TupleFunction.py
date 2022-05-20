af30f1302e5a61431d7677972f2a1ff9ad41e311
mahir <mahirpatel3107@gmail.com>
2022-05-20 23:30:35 +0530

    Diffrent Tuple function

---
+++ Diffrent_TupleFunction.py
@@ -0,0 +1,8 @@
+from operator import le
+
+
+numTuple=(1,2,3,4,5,6,1,1,2,5,3,4,1)
+
+print("The number of 1 in the tuple ==> ",numTuple.count(1))  #count the number present in the tuple
+
+print("The 5 is present on the index : ",numTuple.index(5))  #retrun the index of passed element for searching the position
