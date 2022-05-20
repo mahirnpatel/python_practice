e61141ad5a7ef07f914bca4410d0805291da66ae
mahir <mahirpatel3107@gmail.com>
2022-05-20 23:30:12 +0530

    Diffrent string function

---
+++ Diffrent_string_fun.py
@@ -0,0 +1,17 @@
+from itertools import count
+
+
+name = "Hey how are you my dear friend"
+
+print(len(name))  #length function retrun the number of character in the string
+
+print(name.endswith("Patel")) #endswith function retrun true or false if the string end with the particular provided string 
+
+print(name.count("a"))  #count the number of particular character in the string 
+
+print(name.capitalize())  #Capitalize the first character of the string 
+
+print(name.find("f"))  #find character N in the string return the index of the character
+
+print(name.replace("friend","best friend")) #replace the old word with new word
+
