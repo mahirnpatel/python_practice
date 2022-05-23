2e1d45d6ff3518f75677ef424bcee7e0a923cc48
mahir <mahirpatel3107@gmail.com>
2022-05-24 02:00:53 +0530

    find second highest element from the list

---
+++ SecondHIghestElem_InList.py
@@ -0,0 +1,10 @@
+# Input: list1 = [10, 20, 4]
+# Output: 10
+
+numList = [10,20,4]
+
+numList.sort()
+
+print(numList)
+
+print("The Second highest element in the List ==> " , numList[len(numList)-2])
