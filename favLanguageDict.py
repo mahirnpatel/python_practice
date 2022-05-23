5b1e7c36463c7ea805b5ea4a23a47d973c2d74b3
mahir <mahirpatel3107@gmail.com>
2022-05-24 02:03:47 +0530

    add fav language of the persons

---
+++ favLanguageDict.py
@@ -0,0 +1,19 @@
+favLanguage={
+    "mahir" : "",
+    "raj" : "",
+    "manav" : "",
+    "vatsal" : ""
+}
+
+# print(favLanguage["mahir"])
+mahirFavlLan = input("Enter your fav language : ")
+rajFavlLan = input("Enter your fav language : ")
+manavFavlLan = input("Enter your fav language : ")
+vatsalFavlLan = input("Enter your fav language : ")
+
+favLanguage["mahir"] = mahirFavlLan
+favLanguage["raj"] = rajFavlLan
+favLanguage["manav"] = manavFavlLan
+favLanguage["vatsal"] = vatsalFavlLan
+
+print(favLanguage)
