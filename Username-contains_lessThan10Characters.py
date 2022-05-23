c343e739768f15b80a5e8685fc574a81989401c7
mahir <mahirpatel3107@gmail.com>
2022-05-24 01:58:33 +0530

    check wheather the entered username is less than 10 character or not

---
+++ Username-contains_lessThan10Characters.py
@@ -0,0 +1,6 @@
+userName = input("Enter your username : ")
+
+if(len(userName) < 10):
+    print("It contains less than 10 character")
+else:
+    print("It contain more than 10 character")            
