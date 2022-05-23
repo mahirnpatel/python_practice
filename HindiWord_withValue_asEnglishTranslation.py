396a743564d2474d4fed3d39617eae565e2b8f79
mahir <mahirpatel3107@gmail.com>
2022-05-24 02:03:25 +0530

    translate the meaning of the any word into English

---
+++ HindiWord_withValue_asEnglishTranslation.py
@@ -0,0 +1,8 @@
+hindiWord={
+    "pankha" : "Fan",
+    "Dabba" : "Box",
+    "Vastu" : "Item"
+}
+
+userHindiWord= input("Enter any word : ")
+print("The translation of the word",userHindiWord, " ==> " , hindiWord[userHindiWord])
